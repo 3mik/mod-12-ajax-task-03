b90cdfb8c4f77d7befa4f404ea8aa484f8afd7d7
Krzysztof Milek <krzysztof.milek73@gmail.com>
2018-01-08 00:32:56 +0100

    Add app logic

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = $('#countries');
+
+
+function showCountriesList(resp) {
+    countriesList.empty();           
+    resp.forEach(function(item) {
+        var li = $('<li>');
+        var section = $('<section>');
+        $('<h1><span><img src="' + item.flag + '" alt="Flag"></span>' + item.name +'</h1>').appendTo(section);       
+        $('<p><span>Capital: </span>' + item.capital + '</p>').appendTo(section);
+        $('<p><span>Currency: </span>' + item.currencies[0].code + '</p>').appendTo(section);
+        section.appendTo(li);
+        li.appendTo(countriesList);
+    });
+}
+
+
+
+function searchCountries() {
+    var countryName = $('#country-name').val();
+    if(!countryName.length) countryName = 'Poland';
+    $.ajax({
+        dataType: "json",
+        url: url + countryName,
+        method: 'GET',
+        data: null,
+        success: showCountriesList
+    });
+}
+
+
+$('#search').click(searchCountries);
