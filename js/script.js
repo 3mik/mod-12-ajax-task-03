3773f9e1a221e7c7466c3da2d82b43aef25dd167
Krzysztof Milek <krzysztof.milek73@gmail.com>
2018-01-08 00:38:31 +0100

    Modify showCountriesList function

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function showCountriesList(resp) {
         var li = $('<li>');
         var section = $('<section>');
         $('<h1><span><img src="' + item.flag + '" alt="Flag"></span>' + item.name +'</h1>').appendTo(section);       
+        $('<h3>Background Information:</h3>').appentTo(section);
         $('<p><span>Capital: </span>' + item.capital + '</p>').appendTo(section);
         $('<p><span>Currency: </span>' + item.currencies[0].code + '</p>').appendTo(section);
         section.appendTo(li);
