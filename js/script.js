e56aecf5e6ee76749d3c829d8567a63ead037ffc
Krzysztof Milek <krzysztof.milek73@gmail.com>
2018-01-08 18:18:39 +0100

    Modify showCountriesList function

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ function showCountriesList(resp) {
     countriesList.empty();           
     resp.forEach(function(item) {
         var li = $('<li>');
+        console.log(li);
         var section = $('<section>');
-        $('<h1><span><img src="' + item.flag + '" alt="Flag"></span>' + item.name +'</h1>').appendTo(section);       
-        $('<h3>Background Information:</h3>').appentTo(section);
-        $('<p><span>Capital: </span>' + item.capital + '</p>').appendTo(section);
-        $('<p><span>Currency: </span>' + item.currencies[0].code + '</p>').appendTo(section);
+        console.log(section);
+        $('<h1><span class="desc"><img src="' + item.flag + '" alt="Flag"></span><span class="title">' + item.name +'</span></h1>').appendTo(section);       
+        $('<h3>Background Information:</h3>').appendTo(section);
+        $('<p><span class="desc">Capital: </span><span>' + item.capital + '</span></p>').appendTo(section);
+        $('<p><span class="desc">Currency: </span><span>' + item.currencies[0].code + '</span></p>').appendTo(section);
+        $('<p><span class="desc">Region: </span><span>' + item.region + '</span></p>').appendTo(section);
+        $('<p><span class="desc">Population: </span><span>' + item.population + '</span></p>').appendTo(section);
         section.appendTo(li);
         li.appendTo(countriesList);
     });
